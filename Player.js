3be6773ec70d198b2006fe297a9be1900c19d3a6
PranitP12 <69881672+PranitP12@users.noreply.github.com>
2020-11-15 18:59:22 -0800

    Add files via upload

---
+++ Player.js
@@ -0,0 +1,35 @@
+class Player {
+  constructor(){
+    this.index = null;
+    this.distance = 0;
+    this.name = null;
+  }
+
+  getCount(){
+    var playerCountRef = database.ref('playerCount');
+    playerCountRef.on("value",(data)=>{
+      playerCount = data.val();
+    })
+  }
+
+ updateCount(){
+   database.ref('/').update({
+     playerCount: count
+   });
+ }
+
+  update(){
+    var playerIndex = "player/player" + this.index;
+    database.ref(playerIndex).set({
+      name:this.name,
+      distance:this.distance
+    });
+  }
+
+  static getPlayerInfo(){
+    var playerInfoRef = database.ref('players');
+    playerInfoRef.on("value",(data)=>{
+      allPlayers = data.val();
+    })
+  }
+}
